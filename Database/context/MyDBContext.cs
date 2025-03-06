77f75433460d4f9db61f701e09212994ebbdfbd4
suyser <zagorulkokristina554@gmail.com>
2025-03-06 11:36:27 +0700

    написана пупупуп

--- Database/context/MyDBContext.cs
+++ Database/context/MyDBContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElectricalProspectingProfiling.Database.context
 {
-    class MyDBContext
+    class MyDBContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
